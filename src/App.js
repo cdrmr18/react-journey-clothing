541c2f4a9ab6e72171037b25b87be998943f4b88
Kathy Tavia <kathy.tavia@gmail.com>
2022-08-17 10:01:19 +0200

    added more routes & added firebase

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import Home from "./components/routes/home/home.component";
 import { Routes, Route } from "react-router-dom";
 import Navigation from "./components/routes/navigation/navigation.component";
+import SignIn from "./components/routes/sign-in/sign-in.component";
+import Shop from "./components/routes/shop/shop.component.jsx";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path="/shop" />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   );
