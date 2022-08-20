7b72fefd41e25fcb9e58572ec9f8aac5bc97f344
Kathy Tavia <kathy.tavia@gmail.com>
2022-08-20 22:01:17 +0200

    added cart item & dropdown componet

--- src/components/context/cart.context.jsx
+++ src/components/context/cart.context.jsx
@@ -1,13 +1,40 @@
 import { createContext, useState } from "react";
 
+export const addCartItem = (cartItems, productToAdd) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === productToAdd.id
+  );
+
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === productToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+
+  return [...cartItems, { ...productToAdd, quantity: 1 }];
+};
+
 export const CartContext = createContext({
   isVisible: false,
   setIsVisible: () => {},
+  cartItems: [],
+  addItemToCart: () => {},
 });
 
 export const CartProvider = ({ children }) => {
   const [isVisible, setIsVisible] = useState(false);
+  const [cartItems, setCartItems] = useState([]);
 
-  const value = { isVisible, setIsVisible };
+  const addItemToCart = (product) =>
+    setCartItems(addCartItem(cartItems, product));
+
+  const value = {
+    isVisible,
+    setIsVisible,
+    cartItems,
+    addItemToCart,
+  };
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
