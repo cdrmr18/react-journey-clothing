54f322721c6b625d7726d39679c1e33f8da3eaac
Kathy Tavia <kathy.tavia@gmail.com>
2022-08-24 18:41:35 +0200

    added style & link to product cards

--- src/components/routes/categories-preview/categories-preview.component.jsx
+++ src/components/routes/categories-preview/categories-preview.component.jsx
@@ -1,7 +1,7 @@
 import { useContext, Fragment } from "react";
 
 import { CategoriesContext } from "../../context/categories.context";
-import CategoryPreview from "../../../components/category-preview/category-preview";
+import CategoryPreview from "../../../components/category-preview/category-preview.component";
 
 const CategoriesPreview = () => {
   const { categoriesMap } = useContext(CategoriesContext);
