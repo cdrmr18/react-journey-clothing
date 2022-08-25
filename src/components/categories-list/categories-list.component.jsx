b1584460ca2c3ade5d276a94b79cf4231fcdd8f8
Kathy Tavia <kathy.tavia@gmail.com>
2022-08-25 19:53:24 +0200

    added homepage directory routes

--- src/components/categories-list/categories-list.component.jsx
+++ src/components/categories-list/categories-list.component.jsx
@@ -2,23 +2,37 @@ import DirectoryItem from "../directory-item/directory-item.component";
 import { CategoriesContainer } from "./categories-list.styles.jsx";
 
 const categories = [
-  { id: 1, title: "Hats", imageUrl: "https://i.ibb.co/cvpntL1/hats.png" },
+  {
+    id: 1,
+    title: "Hats",
+    imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+    route: "shop/hats",
+  },
+
   {
     id: 2,
     title: "Jackets",
     imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+    route: "shop/jackets",
   },
   {
     id: 3,
     title: "Sneakers",
     imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+    route: "shop/sneakers",
   },
   {
     id: 4,
     title: "Women's",
     imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+    route: "shop/womens",
   },
-  { id: 5, title: "Men's", imageUrl: "https://i.ibb.co/R70vBrQ/men.png" },
+  {
+    id: 5,
+    title: "Men's",
+    imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+    route: "shop/mens",
+  },
 ];
 
 const CategoriesList = () => {
