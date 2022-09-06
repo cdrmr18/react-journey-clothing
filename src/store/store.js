ce38745fd229031ce2daaab7e50e19cc20a537e3
Kathy Tavia <kathy.tavia@gmail.com>
2022-09-06 11:55:10 +0200

    set middleware for development only

--- src/store/store.js
+++ src/store/store.js
@@ -13,7 +13,9 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [logger];
+const middleWares = [process.env.NODE_ENV === "development" && logger].filter(
+  Boolean
+);
 const composeEnhancers = compose(applyMiddleware(...middleWares));
 export const store = createStore(persistedReducer, undefined, composeEnhancers);
 
