32b962ce9ffd248303ce7555cbbac2d380543150
Kathy Tavia <kathy.tavia@gmail.com>
2022-09-09 19:42:23 +0200

    setting up redux saga

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,8 @@
 import { compose, createStore, applyMiddleware } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
+import createSagaMIddleware from "redux-saga";
+import { rootSaga } from "./root-saga/root-saga";
 
 import logger from "redux-logger";
 import { rootReducer } from "./root-reducer";
@@ -11,12 +13,18 @@ const persistConfig = {
   blacklist: ["user"],
 };
 
+const sagaMiddleware = createSagaMIddleware();
+
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [process.env.NODE_ENV === "development" && logger].filter(
-  Boolean
-);
+const middleWares = [
+  process.env.NODE_ENV === "development" && logger,
+  sagaMiddleware,
+].filter(Boolean);
+
 const composeEnhancers = compose(applyMiddleware(...middleWares));
 export const store = createStore(persistedReducer, undefined, composeEnhancers);
+
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
